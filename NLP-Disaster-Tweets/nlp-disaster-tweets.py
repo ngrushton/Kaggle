5cd10ebacd67d6868fdaa612dd8ea166aefad0fe
Nick Rushton <n.g.rushton@outlook.com>
2020-05-16 16:18:18 +0200

    Updates

--- NLP-Disaster-Tweets/nlp-disaster-tweets.py
+++ NLP-Disaster-Tweets/nlp-disaster-tweets.py
@@ -1,1 +1,12 @@
-print("hello")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+tweet = pd.read_csv('Data/train.csv')
+test = pd.read_csv('Data/test.csv')
+tweet.head(3)
+
+x = tweet.target.value_counts()
+sns.barplot(x.index, x)
+plt.gca().set_ylabel('samples')
